c62dfd5c5bb685468c57f3909035b8f7d004c805
Julian Erville <jerville@gmail.com>
2021-11-17 11:44:41 -0500

    Complete user model portion of backend auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,5 +18,39 @@
 #  created_at          :datetime         not null
 #  updated_at          :datetime         not null
 #
+
 class User < ApplicationRecord
+
+  attr_reader :password
+  after_initialize :ensure_session_token
+
+  validates :email, :password_digest, :session_token, presence: true
+  validates :email, uniqueness: true
+  validates :password, length: { minimum: 8 }, allow_nil: true
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    return user if user && user.is_valid_password?(password)
+    nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_valid_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom::urlsafe_base64
+  end
+
 end
