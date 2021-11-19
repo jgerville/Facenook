b9fb08ea61462707a50ea1811929f40295d7917a
Julian Erville <jerville@gmail.com>
2021-11-19 13:59:59 -0500

    Add reducers for entities:users, session, and errors:sessionErrors

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,3 +1,19 @@
-const sessionReducer = (state = {}, action) => {
-  Object.freeze(state)
+import { LOGOUT_CURRENT_USER, RECEIVE_CURRENT_USER } from "../actions/session_actions";
+
+const nullUser = { id: null }
+Object.freeze(nullUser);
+
+const sessionReducer = (state = nullUser, action) => {
+  Object.freeze(state);
+
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      return { id: action.currentUser.id }
+    case LOGOUT_CURRENT_USER:
+      return nullUser;
+    default:
+      return state;
+  }
 }
+
+export default sessionReducer;
