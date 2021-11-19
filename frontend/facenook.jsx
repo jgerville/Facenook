c072c8bb4a7cbc8e2907136d0bff4fcee21bd6aa
Julian Erville <jerville@gmail.com>
2021-11-19 18:02:35 -0500

    bootstrap current user

--- frontend/facenook.jsx
+++ frontend/facenook.jsx
@@ -5,10 +5,18 @@ import Root from './components/root';
 import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
+  let preloadedState = undefined;
+  if (window.currentUser) {
+    preloadedState = {
+      session: {id: window.currentUser.id },
+      entities: {users: { [window.currentUser.id]: window.currentUser }}
+    }
+  }
 
   const root = document.getElementById('root');
+  const store = configureStore(preloadedState);
+  delete window.currentUser;
 
-  const store = configureStore();
   ReactDOM.render(<Root store={store} />, root)
 
   // testing:
