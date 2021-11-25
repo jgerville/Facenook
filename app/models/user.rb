55d2c1241d61c82b20a0dd8c6ee3bae9f6bbdd01
Julian Erville <jerville@gmail.com>
2021-11-24 23:25:00 -0500

    Add aws connection

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,6 +28,8 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   validates :password, length: { minimum: 8 }, allow_nil: true
 
+  has_one_attached :profpic
+
   def self.find_by_credentials(email, password)
     user = User.find_by(email: email)
     return user if user && user.is_valid_password?(password)
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ActiveRecord::Base.transaction do
+  User.destroy_all
+
+  faker = User.create(email: 'fake@fake.fake', password: 'fakefake', birthday: 'Mar 7 1995', fname: 'Faker', lname: 'McFakerson', gender: 'Male', jobs: 'Very Real Job', schools: 'Mars University', current_city: 'San Francisco', hometown: 'Not Fake City')
+  char = User.create(email: 'char@fire.duels', password: 'fakefake', birthday: 'Aug 23 1975', fname: 'Char', lname: 'Izard', gender: 'Female', jobs: 'Being cool', current_city: 'Cerulean')
+  ash = User.create(email: 'ash@hates.school', password: 'fakefake', birthday: 'May 21 1923', fname: 'Ash', lname: 'Ketchum', gender: 'Male', jobs: 'Pokemon Trainer', current_city: 'Pewter', hometown: 'Pallet Town')
+  peter = User.create(email: 'normalguy@i.promise', password: 'fakefake', birthday: 'April 1 1954', fname: 'Peter', lname: 'Parker', gender: 'Male', jobs: 'nothing special', schools: 'Midtown High School', current_city: 'New York City', hometown: 'New York City')
+end
