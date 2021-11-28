42b75f57c19fdd0c921f0d318af26427eacf4aeb
Julian Erville <jerville@gmail.com>
2021-11-28 11:35:48 -0500

    Add profpics to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'open-uri'
+
 ActiveRecord::Base.transaction do
   User.destroy_all
 
   faker = User.create(email: 'fake@fake.fake', password: 'fakefake', birthday: 'Mar 7 1995', fname: 'Faker', lname: 'McFakerson', gender: 'Male', jobs: 'Very Real Job', schools: 'Mars University', current_city: 'San Francisco', hometown: 'Not Fake City')
+  botpic = URI.open('https://facenook-seeds.s3.us-east-2.amazonaws.com/robot.jpg')
+  faker.profpic.attach(io: botpic, filename: 'faker-avatar.jpg')
+
   char = User.create(email: 'char@fire.duels', password: 'fakefake', birthday: 'Aug 23 1975', fname: 'Char', lname: 'Izard', gender: 'Female', jobs: 'Being cool', current_city: 'Cerulean')
+  charpic = URI.open('https://facenook-seeds.s3.us-east-2.amazonaws.com/char.jpeg')
+  char.profpic.attach(io: charpic, filename: 'char-avatar.jpg')
+
   ash = User.create(email: 'ash@hates.school', password: 'fakefake', birthday: 'May 21 1923', fname: 'Ash', lname: 'Ketchum', gender: 'Male', jobs: 'Pokemon Trainer', current_city: 'Pewter', hometown: 'Pallet Town')
+  ashpic = URI.open('https://facenook-seeds.s3.us-east-2.amazonaws.com/ash.jpg')
+  ash.profpic.attach(io: ashpic, filename: 'ash-avatar.jpg')
+
   peter = User.create(email: 'normalguy@i.promise', password: 'fakefake', birthday: 'April 1 1954', fname: 'Peter', lname: 'Parker', gender: 'Male', jobs: 'nothing special', schools: 'Midtown High School', current_city: 'New York City', hometown: 'New York City')
+  petepic = URI.open('https://facenook-seeds.s3.us-east-2.amazonaws.com/petey.jpg')
+  peter.profpic.attach(io: petepic, filename: 'peter-avatar.jpg')
 end
