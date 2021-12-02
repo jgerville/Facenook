8741f8ab4be946e89083a2bd058849585c88e57b
Julian Erville <jerville@gmail.com>
2021-12-02 10:39:55 -0500

    Make width of each comment only as large as it needs to be to prepare for adding comment footer

---
+++ frontend/components/posts/post/CommentFooter.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const CommentFooter = props => {
+  return (
+    <div className="comment-footer">
+      
+    </div>
+  )
+}
+
+CommentFooter.propTypes = {
+
+}
+
+export default CommentFooter
