b48cd6fd292f674ddef4305c606f49ded3aa223c
Julian Erville <jerville@gmail.com>
2021-12-03 08:42:41 -0500

    Refine TopNav to add GH and flower-type links

--- frontend/components/home/topnav/TopNav.jsx
+++ frontend/components/home/topnav/TopNav.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import AccountDropdownContainer from "./dropdown/account_dropdown_container";
 import HomeAndSearchContainer from "./home_and_search_container";
 
@@ -27,12 +27,21 @@ const TopNav = ({ currentUser, dropdown, openDropdown, closeDropdown }) => {
         <li>
           <HomeAndSearchContainer />
         </li>
-        <li>
-          <Link to="/2">user2</Link>
+      </ul>
+      <ul className="middle-links">
+        <li className="home-link">
+          <NavLink to="/">
+            <i className="fas fa-home" />
+          </NavLink>
         </li>
-        <li className="user-link">
-          <a href="#">{currentUser.fname}</a>
+        <li className="gh-link">
+          <a href="https://github.com/jgerville"><i className="fab fa-github"></i></a>
         </li>
+        <li className="flower-link">
+          <a href="https://flower-type.herokuapp.com/"><i className="fas fa-gamepad"></i></a>
+        </li>
+      </ul>
+      <ul className="dropdown-container">
         <li>
           <div className={isActive ? "badge active" : "badge"} onClick={toggleDropDown}>
             <i className="fas fa-caret-down"/>
