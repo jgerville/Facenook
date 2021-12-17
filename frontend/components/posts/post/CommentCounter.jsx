3621c372673c712e5bcb5f1547689e9e6cec36a0
Julian Erville <jerville@gmail.com>
2021-12-17 11:16:26 -0500

    Include comment replies in top-level comment counts

--- frontend/components/posts/post/CommentCounter.jsx
+++ frontend/components/posts/post/CommentCounter.jsx
@@ -1,21 +1,41 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
-const CommentCounter = ({ post, toggle }) => {
+const CommentCounter = ({ post, posts, toggle }) => {
+  const childCount = () => {
+    const numChildren = post.childPosts.length;
+
+    if (numChildren === 0) {
+      return "0 comments";
+    } else if (numChildren === 1) {
+      return "1 comment";
+    } else {
+      let totalComments = numChildren;
+      for (let i = 0; i < numChildren; i++) {
+        let grandChildrenArray = posts[post.childPosts[i]].childPosts;
+        console.log(post.body, grandChildrenArray)
+        totalComments += grandChildrenArray.length;
+      }
+      return `${totalComments} comments`
+    }
+  }
+
   return (
     <div className="comment-counts" onClick={toggle}>
-      {post.childPosts.length > 0 ? (
-        <span className="has-comments">{post.childPosts.length} comments</span>
-      ) : (
-        <span className="has-no-comments">0 comments</span>
-      )}
+      <span>{childCount()}</span>
     </div>
   );
 };
 
+const mapStateToProps = ({ entities: { posts } }) => ({
+  posts,
+})
+
 CommentCounter.propTypes = {
   post: PropTypes.object.isRequired,
   toggle: PropTypes.func.isRequired,
+  posts: PropTypes.object.isRequired,
 };
 
-export default CommentCounter;
+export default connect(mapStateToProps, null)(CommentCounter);
