754385defd1db9a5cde97f38d95fa957948dffe8
Julian Erville <jerville@gmail.com>
2021-12-17 11:23:16 -0500

    Allow newlines in top-level posts

--- frontend/components/posts/post/PostContent.jsx
+++ frontend/components/posts/post/PostContent.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 const PostContent = ({ body }) => (
   <div className="post-content">
-    <p className="post-body">{body}</p>
+    <pre className="post-body">{body}</pre>
   </div>
 );
 
